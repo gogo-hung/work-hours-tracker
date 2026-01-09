246860dc4041d592d8c5b013a8ec2f2be9ac9eae
gogo-hung <abon091868@gmail.com>
2026-01-09 21:44:17 +0800

    Fix postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,4 @@ export default {
     tailwindcss: {},
     autoprefixer: {},
   },
-}git remote add origin https://github.com/你的用戶名/work-hours-tracker.git
-git branch -M main
-git push -u origin main
+}
